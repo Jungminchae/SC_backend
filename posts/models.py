fbc2ae9fd1fc92f5c879480f81976655a6b35032
Jungminchae <minchae3618@gmail.com>
2021-11-09 23:19:06 +0900

    ADD: Video Photo 모델 추

--- posts/models.py
+++ posts/models.py
@@ -2,21 +2,37 @@ from django.db import models
 from core.models import TimeStampModel
 
 
-class Post(TimeStampModel):
+class KnowHowPost(TimeStampModel):
     user = models.ForeignKey(
-        "accounts.Profile", related_name="post_profile", on_delete=models.CASCADE
+        "users.Profile", related_name="knowhow_profile", on_delete=models.CASCADE
     )
     title = models.CharField(max_length=100)
     content = models.TextField()
-    like = models.ManyToManyField("accounts.Profile", related_name="like")
+    like = models.ManyToManyField("users.Profile", related_name="like")
 
 
-class PostImage(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="post_image")
-    image = models.ImageField(blank=True, null=True, upload_to="posts/images/%Y/%m/%d")
+class KnowHowPostImage(models.Model):
+    post = models.ForeignKey(
+        KnowHowPost, on_delete=models.CASCADE, related_name="knowhow_image"
+    )
+    image = models.ImageField(
+        blank=True, null=True, upload_to="knowhow/images/%Y/%m/%d"
+    )
+
+
+class Photo(TimeStampModel):
+    user = models.ManyToManyField("users.Profile", related_name="photo")
+    photo = models.ImageField(upload_to="photo/images/%Y/%m/%d")
+    description = models.CharField(max_length=255, default="")
+
+
+class Video(TimeStampModel):
+    user = models.ManyToManyField("users.Profile", related_name="video")
+    video = models.FileField(upload_to="video/videos/%Y/%m/%d")
+    description = models.CharField(max_length=255, default="")
 
 
 class Bookmark(TimeStampModel):
-    user = models.ManyToManyField("accounts.Profile", related_name="bookmark")
-    name = models.CharField(default="나의 북마크")
+    user = models.ManyToManyField("users.Profile", related_name="bookmark")
+    name = models.CharField(max_length=20, default="나의 북마크")
     urls = models.URLField()
