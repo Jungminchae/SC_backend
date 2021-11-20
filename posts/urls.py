cfa7fdc0f541ae60b101dbae0001a03407c6f8d3
JungMinChae <60789129+Jungminchae@users.noreply.github.com>
2021-11-20 15:43:48 +0900

    ADD 사진, 동영상, 북마크 view (#5)
    * 사진,동영상, 북마크 관련 viewset, serializer 추가
    * 관련 권한 수정
    * 다중이미지 업로드 수정

--- posts/urls.py
+++ posts/urls.py
@@ -1,9 +1,12 @@
 # from django.urls import path
 from rest_framework.routers import DefaultRouter
-from .views import KnowHowViewSet
+from .views import KnowHowViewSet, PhotoViewSet, VideoViewSet
 
 router = DefaultRouter()
-router.register("", KnowHowViewSet)
+router.register("knowhows/", KnowHowViewSet)
+router.register("photos/", PhotoViewSet)
+router.register("videos/", VideoViewSet)
+
 urlpatterns = []
 
 urlpatterns += router.urls
--- posts/views.py
+++ posts/views.py
@@ -2,38 +2,47 @@ from django.db.models import Q
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.decorators import action
 from rest_framework.response import Response
-from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.permissions import AllowAny
 from rest_framework import status
-from .models import KnowHowPost
-from .serializers import KnowHowPostSerializer
+from .models import KnowHowPost, Photo, Video, Bookmark
+from .serializers import (
+    KnowHowPostSerializer,
+    PhotoSerializer,
+    VideoSerializer,
+    BookMarkSerializer,
+)
 from .permissions import IsMe
 
 
-# TODO
-# User 모델이랑 Profile모델이랑 헷갈림 수정 필요
+# TODO: 권한 설정
+# 글보기: 누구나 가능, 글쓰기: 삭제, 수정: 나만 가능
 class KnowHowViewSet(ModelViewSet):
-
     queryset = KnowHowPost.objects.all()
     serializer_class = KnowHowPostSerializer
 
     def get_permissions(self):
         if self.action == "list" or self.action == "retrieve":
             permission_classes = [AllowAny]
-        elif self.action == "create":
-            permission_classes = [IsAuthenticated]
         else:
             permission_classes = [IsMe]
         return [permission() for permission in permission_classes]
 
+    def get_serializer_context(self):
+        context = super().get_serializer_context()
+        context["request"] = self.request
+        return context
+
     # Knowhow 쓰기
     def create(self, request):
-        # 로그인 상태의 유저만 질문하기 가능 - 기본 permission IsAuthenticated
-        if not request.user.is_authenticated:
-            return Response(status=status.HTTP_401_UNAUTHORIZED)
-        serializer = KnowHowPostSerializer(data=request.data)
+        # 로그인 상태의 유저만 질문하기 가능 - 기본 permission IsAuthenticaåted
+        serializer = KnowHowPostSerializer(
+            data=request.data, context={"request": request}
+        )
         # 유효성검사
         if serializer.is_valid():
-            knowhow = serializer.save(user=request.user)
+            knowhow = serializer.save(
+                user=request.user,
+            )
             knowhow_serializer = KnowHowPostSerializer(knowhow).data
             return Response(data=knowhow_serializer, status=status.HTTP_201_CREATED)
         else:
@@ -57,3 +66,34 @@ class KnowHowViewSet(ModelViewSet):
             return paginator.get_paginated_response(serializer)
         else:
             return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class PhotoViewSet(ModelViewSet):
+    queryset = Photo.objects.all()
+    serializer_class = PhotoSerializer
+
+    def get_permissions(self):
+        if self.action == "list" or self.action == "retrieve":
+            permission_classes = [AllowAny]
+        else:
+            permission_classes = [IsMe]
+        return [permission() for permission in permission_classes]
+
+
+class VideoViewSet(ModelViewSet):
+    queryset = Video.objects.all()
+    serializer_class = VideoSerializer
+
+    def get_permissions(self):
+        if self.action == "list" or self.action == "retrieve":
+            permission_classes = [AllowAny]
+        else:
+            permission_classes = [IsMe]
+        return [permission() for permission in permission_classes]
+
+
+# Bookmark 보기, 생성, 수정, 삭제 나만 가능
+class BookMarkViewSet(ModelViewSet):
+    queryset = Bookmark
+    serializer_class = BookMarkSerializer
+    permission_classes = [IsMe]
