3a58bb5d94b3b2a42995ec99c1d160a20b5c630b
Jungminchae <minchae3618@gmail.com>
2021-11-29 21:40:33 +0900

    ADD like and FIX Video Viewset

--- posts/urls.py
+++ posts/urls.py
@@ -1,12 +1,23 @@
-# from django.urls import path
+from django.urls import path
 from rest_framework.routers import DefaultRouter
-from .views import KnowHowViewSet, PhotoViewSet, VideoViewSet
+from .views import (
+    KnowHowViewSet,
+    PhotoViewSet,
+    VideoViewSet,
+    like_knowhow,
+    like_photo,
+    like_video,
+)
 
 router = DefaultRouter()
 router.register("knowhows", KnowHowViewSet)
 router.register("photos", PhotoViewSet)
 router.register("videos", VideoViewSet)
 
-urlpatterns = []
+urlpatterns = [
+    path("knowhows/<int:pk>", like_knowhow, name="like-or-unlike-knowhow"),
+    path("photos/<int:pk>", like_photo, name="like-or-unlike-photo"),
+    path("videos/<int:pk>", like_video, name="like-or-unlike-video"),
+]
 
 urlpatterns += router.urls
