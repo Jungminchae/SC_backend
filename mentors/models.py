3d829439965e96217f2ffc6541c733def2b2ae84
Jungminchae <minchae3618@gmail.com>
2021-12-04 13:12:14 +0900

    ADD mentor 기본 셋업

--- mentors/models.py
+++ mentors/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.conf import settings
+from core.models import TimeStampModel
 
-# Create your models here.
+
+# TODO: 멘토 항목은 정해지면 개발
+class Mentor(TimeStampModel):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="mentor"
+    )
--- mentors/urls.py
+++ mentors/urls.py
@@ -1,1 +1,5 @@
-urlpatterns = []
+from django.urls import path
+from mentors.views import MentorView
+
+
+urlpatterns = [path("", MentorView.as_view(), name="mentor")]
