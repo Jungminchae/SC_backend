a1f0883d56239a4669b4729e2e13233137b864ed
Jungminchae <minchae3618@gmail.com>
2022-02-17 09:29:56 +0000

    FIX edit comment serializer

--- comments/serializers.py
+++ comments/serializers.py
@@ -19,17 +19,28 @@ class KnowHowCommentSerializer(serializers.ModelSerializer):
 
 class PhotoCommentSerializer(serializers.ModelSerializer):
     profile = ProfileSerializer(read_only=True)
+    reply = serializers.SerializerMethodField()
 
     class Meta:
         model = PhotoComment
-        fields = ("id", "profile", "comment", "created_at", "updated_at")
+        fields = ("id", "profile", "comment", "created_at", "updated_at", "reply")
         read_only_fields = ("id", "created_at", "updated_at")
 
+    def get_reply(self, instance):
+        serializer = self.__class__(instance.replies, many=True)
+        serializer.bind("", self)
+        return serializer.data
 
 class VideoCommentSerializer(serializers.ModelSerializer):
     profile = ProfileSerializer(read_only=True)
+    reply = serializers.SerializerMethodField()
 
     class Meta:
         model = VideoComment
-        fields = ("id", "profile", "comment", "created_at", "updated_at")
+        fields = ("id", "profile", "comment", "created_at", "updated_at", "reply")
         read_only_fields = ("id", "created_at", "updated_at")
+        
+    def get_reply(self, instance):
+        serializer = self.__class__(instance.replies, many=True)
+        serializer.bind("", self)
+        return serializer.data
